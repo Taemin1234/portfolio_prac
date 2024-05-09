4fe9ade03a99be819b34739ac7f985446d3e1e67
TM Book <cestmoi7531@gmail.com>
2024-05-09 21:43:11 +0900

    intersecting observer 진행중

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Container from './component/Container'
 import GlobalStyle from './component/GlobalStyle';
 
 function App() {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const targetRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsIntersecting(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.3,
+      }
+    );
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+
+    return () => {
+      if (targetRef.current) {
+        observer.unobserve(targetRef.current);
+      }
+    };
+  }, []);
+
   return (
     <>
       <GlobalStyle />
-      <Container/>
+      <Container ref={targetRef} isIntersecting={isIntersecting}/>
     </>
   );
 }
